39ef58e657ab13cbae607e715169c13f324b8221
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2025-08-28 18:27:20 +0600

    added  copytext section dynamic and copy count

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+
+//  getId
 function getElement(id) {
   return  document.getElementById(id);
 }
- 
+//  get innerText
 function getInnerText(avalibleCoin) {
     return avalibleCoin.innerText
 }
+
+
+
 // love count dynamic section start
       
     const heartBtns =document.getElementsByClassName('heartBtn');
@@ -23,49 +28,21 @@ let count=0;
 
 
 
+//  function coin update
 
-// call section
-
-  const callBtns=document.getElementsByClassName('cal-btn');
-  
- for(const callbtn of callBtns)
-      {
-         callbtn.addEventListener('click',function(e) {
-          
-          
-
-     
-
-  const title=   callbtn.parentElement.parentElement.children[1].querySelector('.title')
-           const titleText=title.innerText;
-           console.log(titleText)
-           
-         const callNumber=  callbtn.parentElement.parentElement.children[1].querySelector('.callNumber')
-         const callNum=callNumber.innerText
-          const time=new Date().toLocaleTimeString();
-       console.log(time);
-
-         console.log(callNum)
-
-  const avalibleCoin = getElement('avaliblecoin');
-    let avalibleCoinInner = Number(getInnerText(avalibleCoin));
-
-        if(avalibleCoinInner <= 100) {
-            alert(`📞 ${titleText} ${callNum}`);
+function reduceCoinUpdate(avalibleCoinInner,avalibleCoin) {
+       
           avalibleCoinInner =   avalibleCoinInner - 20;  // Deduct coins
-            avalibleCoin.innerText = avalibleCoinInner;
-        } else {
-            alert("Not enough coins!");
-        } 
-    
+           return avalibleCoin.innerText = avalibleCoinInner;
+}
 
-
-       const container=getElement('history-container');
+function createCallHistory(titleText,callNum,time) {
+   const container=getElement('history-container');
              
 
        const div=document.createElement('div');
 
-       div.innerHTML=`  <div class="bg-gray-100 py-5 px-3 rounded-lg shadow-md flex justify-between items-center">
+       div.innerHTML=`  <div class="bg-[#fafafa] py-5 px-3 rounded-lg shadow-md flex justify-between items-center">
       <!-- Left: title + number -->
       <div class="flex flex-col">
         <h1 class="text-[#111111] text-lg font-semibold text-ellipsis ">
@@ -82,6 +59,59 @@ let count=0;
 
           container.appendChild(div);
 
+}
+
+// 
+
+// clearBtn function
+function clearBtn() {
+   const clearBtn= getElement('clearBtn').addEventListener('click',function () {
+      const container=getElement('history-container');
+      container.innerHTML='';
+   })
+
+
+}
+// clear btn function call
+clearBtn()
+
+
+// call section
+
+  const callBtns=document.getElementsByClassName('cal-btn');
+  
+ for(const callbtn of callBtns)
+      {
+         callbtn.addEventListener('click',function(e) {
+          
+      
+    //  dom traversing text titile and call number  start here section
+
+     const title= callbtn.parentElement.parentElement.children[1].querySelector('.title')
+           const titleText=title.innerText;
+         
+           
+         const callNumber=  callbtn.parentElement.parentElement.children[1].querySelector('.callNumber')
+         const callNum=callNumber.innerText
+        //  local time string 
+          const time=new Date().toLocaleTimeString();
+        
+    //  dom traversing text titile and call number end here section
+      
+          //  avalible conin
+          const avalibleCoin = getElement('avaliblecoin');
+           let avalibleCoinInner = Number(getInnerText(avalibleCoin));
+                       
+        if(avalibleCoinInner >=20) {
+            
+            alert(`📞${titleText} ${callNum}`);
+            reduceCoinUpdate(avalibleCoinInner,avalibleCoin);
+            createCallHistory(titleText,callNum,time);
+        } else {
+            alert("❌Not enough coins!");
+        } 
+    
+
             
          })
 
@@ -89,4 +119,31 @@ let count=0;
 
       }
       
+        let copyCount=0;
+
+      // copy function
+   function copyTextandCountCopy() {
+     const copyCountBtn=document.getElementById('copyCount')
+    const copyBtns=  document.getElementsByClassName('copBtn')
+  
+      
+      for(const copyBtn of copyBtns)
+      
+      copyBtn.addEventListener('click',function() {
+
+         console.log('bondhu clicked');
+           copyCount++;
+             copyCountBtn.innerText=copyCount;
+             
+             const callNumber=copyBtn.parentElement.parentElement.children[1].children[2]
+             const callNumCopy=callNumber.innerText;
+
+          navigator.clipboard.writeText(callNumCopy)
+            alert(`✅Copied ${callNumCopy}`)
+                // Toast style copy feedback
+      
+        
+      })
+   }
          
+copyTextandCountCopy()
